f3ff188617225602f16daa68108124a10383937b
ittoolsmaker <117898394+ittoolsmaker@users.noreply.github.com>
2024-02-19 21:58:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -63,23 +63,23 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
+  botname: process.env.BotName || "༄●⃝Pʀᴀᴊᴀᴘᴀᴛɪ Oғғɪᴄɪᴀʟ༄", 
+  author: process.env.Author || "@Devil_Saroj",
+  packname: process.env.PackName || "Devil_Hacker v2 MD",
   socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
-  prefa: process.env.Prefix || ['-'],
-  themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
+  footer: process.env.Footer || "© Devil_Saroj Bot",
+  prefa: process.env.Prefix || ['.'],
+  themeemoji: process.env.ThemeEmoji || "Devil_🎐",
+  ownername: process.env.Owner_Name || "༄●⃝Pʀᴀᴊᴀᴘᴀᴛɪ Oғғɪᴄɪᴀʟ༄",
+  ownernumber: process.env.Owner_Number || "917091650914",
   instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+  mongoUrl: process.env.MongoDB || "mongodb+srv://devilhacker:devil1234@cluster0.reri6rw.mongodb.net/?retryWrites=true&w=majority",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
+  sessionId: process.env.SESSION_ID || "bng3YXZ_AN_YA_lZ_AN_YA_Ws=", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
@@ -105,19 +105,19 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.botname = process.env.BotName || "༄●⃝Pʀᴀᴊᴀᴘᴀᴛɪ Oғғɪᴄɪᴀʟ༄" 
+global.author = process.env.Author || "@Devil_Saroj" 
+global.packname = process.env.PackName || "Devil_ Hacker v2 MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
-global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
-global.adress = process.env.Continent || "Asia, India, Assam" 
+global.footer = process.env.Footer || "© Devil_ Saroj Bot" 
+global.prefa = process.env.Prefix || ['.'] 
+global.themeemoji = process.env.ThemeEmoji || "Devil_🎐" 
+global.ownername = process.env.Owner_Name || "༄●⃝Pʀᴀᴊᴀᴘᴀᴛɪ Oғғɪᴄɪᴀʟ༄" 
+global.ownernumber = process.env.Owner_Number || "917091650914" 
+global.adress = process.env.Continent || "Asia, India, New Delhi" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
 global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "example@example.com" 
+global.email = process.env.Email_Id || "jisaroj318@gmail.com" 
   
 //--------------- Tip ----------------\\
 global.Tips = [
